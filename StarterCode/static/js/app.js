15cd67b3286e17b95fb3a7a9b13cc540d5779788
Sarah Shelden <sarah.shelden113@gmail.com>
2019-03-19 12:08:16 -0400

    psuedo code started

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,28 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// append code to dom and have it add new rows of ufo sightings
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
